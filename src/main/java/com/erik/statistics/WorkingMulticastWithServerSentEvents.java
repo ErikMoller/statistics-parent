0721b6c6e126851e3a15552d3fc3682491b9fbd2
erimol <erik.moller@so4it.com>
2016-02-19 20:16:18 +0100

    Refactoring of handlers

--- src/main/java/com/erik/statistics/WorkingMulticastWithServerSentEvents.java
+++ src/main/java/com/erik/statistics/WorkingMulticastWithServerSentEvents.java
@@ -1,6 +1,8 @@
 package com.erik.statistics;
 
+import ratpack.func.Action;
 import ratpack.handling.Chain;
+import ratpack.handling.Handler;
 import ratpack.server.BaseDir;
 import ratpack.server.RatpackServer;
 import ratpack.sse.ServerSentEvents;
@@ -18,40 +20,71 @@ import static ratpack.sse.ServerSentEvents.serverSentEvents;
  * Created by erimol on 2016-02-06.
  */
 public class WorkingMulticastWithServerSentEvents {
+    private static Data data = new Data();
+    private static TransformablePublisher<Object> multicast;
+
     public static void main(String[] args) throws Exception {
-        Data data = new Data();
-        TransformablePublisher<Object> multicast = Streams.multicast(subscriber -> {
-
-                        while (!data.isClosed()) {
-                            try {
-                                subscriber.onNext(data.getData());
-                            } catch (InterruptedException e) {
-                                e.printStackTrace();
-                            }
-                        }
-
-        }).gate(Runnable::run);
+        initMulticast();
         RatpackServer.start(s ->  s.serverConfig(c -> c.baseDir(BaseDir.find()))
                 .handlers(chain ->
                         chain.prefix("static",chain1-> chain1.fileSystem("assets", Chain::files))
-                                .get("subscribe",context -> {
-                            ServerSentEvents events = serverSentEvents(multicast, e ->
-                                    e.id(Objects::toString).event("counter").data(i -> ""+i)
-                            );
-                            context.render(events);
-                            data.addData("Listening on multisocket channel");
-                        }).get("push/:data",ctx -> {
-                                    String input = ctx.getPathTokens().get("data");
-                                    data.addData(input);
-                                    ctx.getResponse().status(200).send();
-                        }).get("start",ctx -> {
-                            ctx.getResponse().contentType("html").sendFile(Paths.get("src/main/resources/html/", "index.html"));
-                        }).get("search/:searchString", ctx -> {
-                            String searchString = ctx.getPathTokens().get("searchString");
-                            ctx.render("Result from server: " + searchString);
-                        }).get("learnAngular", ctx -> {
-                            ctx.getResponse().contentType("html").sendFile(Paths.get("src/main/resources/html/", "learnAngular.html"));
-                        })));
+                                .get("subscribe",subscribeHandler())
+                                .get("push/:data",pushHandler())
+                                .get("start",indexHandler())
+                                .get("search/:searchString", searchHandler())
+                                .get("learnAngular", learnAngluarHandler()))
+        );
+    }
+
+    private static void initMulticast() {
+        multicast = Streams.multicast(subscriber -> {
+
+            while (!data.isClosed()) {
+                try {
+                    subscriber.onNext(data.getData());
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        }).gate(Runnable::run);
+    }
+
+    private static Handler subscribeHandler() {
+        return context -> {
+            ServerSentEvents events = serverSentEvents(multicast, e ->
+                    e.id(Objects::toString).event("counter").data(i -> "" + i)
+            );
+            context.render(events);
+            data.addData("Listening on multisocket channel");
+        };
+    }
+
+    private static Handler pushHandler() {
+        return ctx -> {
+            String input = ctx.getPathTokens().get("data");
+            data.addData(input);
+            ctx.getResponse().status(200).send();
+        };
+    }
+
+    private static Handler indexHandler() {
+        return ctx -> {
+            ctx.getResponse().contentType("html").sendFile(Paths.get("src/main/resources/html/", "index.html"));
+        };
+    }
+
+    private static Handler searchHandler() {
+        return ctx -> {
+            String searchString = ctx.getPathTokens().get("searchString");
+            ctx.render("Result from server: " + searchString);
+        };
+    }
+
+    private static Handler learnAngluarHandler() {
+        return ctx -> {
+            ctx.getResponse().contentType("html").sendFile(Paths.get("src/main/resources/html/", "learnAngular.html"));
+        };
     }
 
 
